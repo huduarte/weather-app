1775a0931ba47a7a1194d914bd981df778537e9b
Hudson <hudson.neto@sinqia.com.br>
2022-01-05 10:20:55 -0300

    feat: adicionando path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.jsx',
+            '.json'
+          ],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
