8bacf9f99cde1e6b4f07573a440b4fe475d925b8
Hudson <hudsoneeto@outlook.com>
2022-01-07 00:10:39 -0300

    feat: tela de Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,115 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-import { Text, View } from 'react-native'
+import {
+  Container, 
+  City, 
+  Temp, 
+  Header, 
+  Data, 
+  Description, 
+  Temperature, 
+  Celsius, 
+  WeatherData,
+  Button,
+} from './styles'
 
-import {Container} from './styles'
+import WeatherAnimation from '@components/WeatherAnimation'
 
-const Home = (): JSX.Element => {
+import LargeButton from '@components/LargeButton'
+import WeatherCard from '@components/WeatherCard'
+import Loader from '@components/Loader'
+import { IResolveWeather, ResolveWeather } from '@utils/resolvers'
+import { convertKelvinToCelsius } from '@utils/converter'
+import { useWeather } from '@hooks/weather'
+
+import { RootStackParamList } from '@routes/stack.routes'
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { Alert } from 'react-native'
+type MainScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Main'
+>;
+
+type Props = {
+  route: {
+    params: {
+      day: 'day' | 'night'
+    }
+  }
+  navigation: MainScreenNavigationProp;
+};
+
+const Home = ({ route, navigation }: Props): JSX.Element => {
+  const [resolvedWeather, setResolvedWeather] = useState({} as IResolveWeather)
+  const [dayPeriod, setDayPeriod] = useState<'day' | 'night'>('day')
+
+  const {day} = route.params
+
+  const {
+    weatherData,
+    updateWeatherData
+  } = useWeather()
+
+  useEffect(() => {
+    setDayPeriod(day)
+  }, [])
+
+  const handlePressUpdateData = async () => {
+    updateWeatherData().catch(() =>{
+      Alert.alert('Ops!', 'Houve um problema ao carregar os dados, por favor, tente novamente!')
+      navigation.popToTop()
+    })
+  }
+
+  useEffect(() => {
+    if(weatherData){
+      const resolvedWeather = ResolveWeather(weatherData?.weather[0]?.id, dayPeriod)
+      setResolvedWeather(resolvedWeather)
+    }
+  }, [weatherData])
+
   return (
-    <Container>
-      <Text>Teste path mapping</Text>
+    <Container type={dayPeriod} colors={[]}>
+      {!weatherData && (
+        <Loader dayPeriod={dayPeriod} />
+      )}
+      {weatherData && (
+        <Header>
+          <Data>
+            <City>{weatherData.name}</City>
+            <Temperature>
+              <Temp>{convertKelvinToCelsius(weatherData.main.temp)}</Temp>
+              <Celsius>°</Celsius>
+            </Temperature>
+          </Data>
+
+          <Description>{weatherData.weather[0].description}</Description>
+        </Header>
+      )}
+
+      {weatherData && resolvedWeather.animation && 
+        <WeatherAnimation 
+          source={resolvedWeather.animation} 
+          description={resolvedWeather.description} 
+        />
+      }
+
+      {weatherData && (
+        <>
+          <WeatherData>
+            <WeatherCard 
+              humidity={weatherData.main.humidity}
+              pressure={weatherData?.main.pressure}
+              windSpeed={weatherData?.wind.speed}
+            />
+          </WeatherData>
+          <Button>
+            <LargeButton title="Atualizar os dados" type={dayPeriod} onPress={handlePressUpdateData} />
+          </Button>
+        </>
+      )}
+
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,65 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+import { LinearGradient, LinearGradientProps } from 'expo-linear-gradient'
+
+type ContainerProps = LinearGradientProps & {
+  type: 'day' | 'night'
+}
+
+export const Container = styled(LinearGradient).attrs<ContainerProps>(({theme, type}) => ({
+  colors: type === 'day' ? theme.COLORS.DAY_GRADIENT : theme.COLORS.NIGHT_GRADIENT,
+  start: {x:0.8 , y:0.1},
+}))<ContainerProps>`
   flex: 1;
-  background-color: blue;
+`
+export const Header = styled.View`
+  flex-direction: row;
+  padding-left: 20px;
+  margin-top: 25%;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const City = styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-size: 24px;
+  font-family: ${({theme}) => theme.FONTS.REGULAR};
+`
+
+export const Temp = styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-size: 72px;
+  font-family: ${({theme}) => theme.FONTS.BOLD};
+`
+
+export const Data = styled.View``
+
+export const Description= styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-family: ${({theme}) => theme.FONTS.BOLD};
+  font-size: 18px;
+  transform: rotate(270deg);
+`
+
+export const Temperature = styled.View`
+  flex-direction: row;
+`
+
+export const Celsius = styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-size: 30px;
+  font-family: ${({theme}) => theme.FONTS.BOLD};
+`
+
+export const WeatherData = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px 0 20px;
+  margin-top: 20%;
+`
+
+export const Button = styled.View`
+  padding-left: 25px;
+  padding-right: 25px;
+  margin-top: 10%;
 `
