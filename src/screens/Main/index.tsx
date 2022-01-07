d18a4f90b35b1ba80cff88e84e4d1a5839eb16e9
Hudson <hudsoneeto@outlook.com>
2022-01-07 00:10:49 -0300

    feat: tela principal

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import LargeButton from '@components/LargeButton'
+import Loader from '@components/Loader'
+import { getDayPeriod } from '@utils/getDayPeriod'
+
+import {
+  Container,
+  Header,
+  Title,
+  Description,
+  Button,
+  Footer,
+} from './styles'
+
+import { useWeather } from '@hooks/weather'
+
+import { RoutesNames } from '@utils/enum'
+
+import { RootStackParamList } from '@routes/stack.routes'
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { Alert } from 'react-native'
+type MainScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Main'
+>;
+
+type Props = {
+  navigation: MainScreenNavigationProp;
+};
+
+const Main = ({ navigation }: Props): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [dayPeriod, setDayPeriod] = useState<'day' | 'night'>('day')
+
+  const handlePressContinue = async () => {
+    setIsLoading(true)
+    await getCurrentLocation().then(() => {
+      console.log('a')
+      navigation.navigate(RoutesNames.HOME, {day: dayPeriod})
+    }).catch(() => {
+      Alert.alert('Ops!', 'Houve um problema ao carregar os dados, por favor, tente novamente!')
+    }).finally(() => {
+      setIsLoading(false)
+    })
+  }
+
+  const {
+    getLocationPermission,
+    getCurrentLocation,
+  } = useWeather()
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await getLocationPermission()
+    }
+
+    fetchData()
+    const hour = new Date().getHours()
+    const dayPeriod = getDayPeriod(hour)
+    setDayPeriod(dayPeriod)
+
+  }, [])
+
+  return (
+    <Container type={dayPeriod} colors={[]}>
+      {isLoading ? (
+        <Loader dayPeriod={dayPeriod} />
+      ) : (
+        <>
+          <Header>
+            <Title>Bem-vindo (a)!</Title>
+            <Description>Clique no botão abaixo e veja as informações sobre o clima da sua localidade</Description>
+          </Header>
+
+          <Button>
+            <LargeButton title="Começar" type={dayPeriod} onPress={handlePressContinue}/>
+          </Button>
+
+
+          <Footer>
+            <Description>Não esqueça de permitir o acesso a sua localização atual.</Description>
+          </Footer>
+        </>
+      )}
+    </Container>
+  )
+}
+
+export default Main
---
+++ src/screens/Main/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native'
+import { LinearGradient, LinearGradientProps } from 'expo-linear-gradient'
+
+type ContainerProps = LinearGradientProps & {
+  type: 'day' | 'night'
+}
+
+export const Container = styled(LinearGradient).attrs<ContainerProps>(({theme, type}) => ({
+  colors: type === 'day' ? theme.COLORS.DAY_GRADIENT : theme.COLORS.NIGHT_GRADIENT,
+  start: {x:0.8 , y:0.1},
+}))<ContainerProps>`
+  flex: 1;
+  justify-content: space-around;
+`
+
+export const Header = styled.View`
+  padding-left: 25px;
+  padding-right: 25px;
+`
+
+export const Title = styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-size: 48px;
+  font-family: ${({theme}) => theme.FONTS.BOLD};
+`
+
+export const Description = styled.Text`
+  color: ${({theme}) => theme.COLORS.TITLE};
+  font-size: 18px;
+  font-family: ${({theme}) => theme.FONTS.REGULAR};
+`
+
+export const Button = styled.View`
+  padding-left: 25px;
+  padding-right: 25px;
+`
+
+export const Footer = styled.View`
+  padding-left: 25px;
+  padding-right: 25px;
+`
